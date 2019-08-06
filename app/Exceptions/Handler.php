aa132a58e204ac04900bf8bbebdbc0b5b5081633
yahyaizhar <yahyaizhar@gmail.com>
2019-08-06 23:54:16 +0500

    exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -62,32 +62,32 @@ class Handler extends ExceptionHandler
         
         // If the request wants JSON (AJAX doesn't always want JSON)
         // Define the response
-        $response = [
-            'errors' => 'Sorry, something went wrong.'
-        ];
+        // $response = [
+        //     'errors' => 'Sorry, something went wrong.'
+        // ];
 
-        // If the app is in debug mode
-        if (true) {
-            // Add the exception class name, message and stack trace to response
-            $response['exception'] = get_class($e); // Reflection might be better here
-            $response['message'] = $e->getMessage();
-            $response['trace'] = $e->getTrace();
-        }
+        // // If the app is in debug mode
+        // if (true) {
+        //     // Add the exception class name, message and stack trace to response
+        //     $response['exception'] = get_class($e); // Reflection might be better here
+        //     $response['message'] = $e->getMessage();
+        //     $response['trace'] = $e->getTrace();
+        // }
 
-        // Default response of 400
-        $status = 400;
+        // // Default response of 400
+        // $status = 400;
 
-        // If this exception is an instance of HttpException
-        if ($this->isHttpException($e)) {
-            // Grab the HTTP status code from the Exception
-            $status = $e->getStatusCode();
-        }
+        // // If this exception is an instance of HttpException
+        // if ($this->isHttpException($e)) {
+        //     // Grab the HTTP status code from the Exception
+        //     $status = $e->getStatusCode();
+        // }
 
-        // Return a JSON response with the response array and status code
-        return response()->json($response, $status);
+        // // Return a JSON response with the response array and status code
+        // return response()->json($response, $status);
 
         // Default to the parent class' implementation of handler
-        // return parent::render($request, $e);
+         return parent::render($request, $e);
     }
     /**
      * Convert an authentication exception into a response.
