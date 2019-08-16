6754354c1a91d3c5f9f6f386ea6d6d21532dc943
yahyaizhar <yahyaizhar@gmail.com>
2019-08-16 19:33:43 +0500

    changes (GDrive -2)

--- config/backup.php
+++ config/backup.php
@@ -204,7 +204,7 @@ return [
              * After cleaning up the backups remove the oldest backup until
              * this amount of megabytes has been reached.
              */
-            'deleteOldestBackupsWhenUsingMoreMegabytesThan' => 5000,
+            'deleteOldestBackupsWhenUsingMoreMegabytesThan' => 100,
         ],
     ],
 ];
