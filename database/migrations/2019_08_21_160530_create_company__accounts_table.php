0e6d2faa421cc03fc4b48af1ac3bc3e8fc5f5153
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2019-08-21 21:24:43 +0500

    merge new databse tables

---
+++ database/migrations/2019_08_21_160530_create_company__accounts_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompanyAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('company__accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('type')->nullable();
+            $table->string('amount')->nullable();
+            $table->integer('rider_id')->nullable();
+            $table->integer('client_id')->nullable();
+            $table->integer('bike_expense_id')->nullable();
+            $table->integer('fine_id')->nullable();
+            $table->integer('salik_id')->nullable();
+            $table->integer('sallary_id')->nullable();
+            $table->integer('sim_transaction_id')->nullable();
+            $table->integer('income_id')->nullable();
+            $table->integer('investment_id')->nullable();
+            $table->integer('mobile_investment_id')->nullable();
+            $table->string('status')->nullable();
+            $table->string('active_status')->default("A");
+            $table->string('setting')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('company__accounts');
+    }
+}
