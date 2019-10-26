50c76e4155638fc2d5dabd53698305bb8e20656f
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2019-10-26 20:21:17 +0500

    maybe not

--- app/Http/Controllers/Admin/AjaxNewController.php
+++ app/Http/Controllers/Admin/AjaxNewController.php
@@ -1787,11 +1787,11 @@ class AjaxNewController extends Controller
             ->addColumn('bike_rent', function($riders){
                 $date_arr=[];
                 $zomato=Rider_Performance_Zomato::whereMonth("date","10")->where("feid",$riders->client_rider_id)->get();
-                $i=0;
+                // $i=0;
                 foreach ($zomato as $item) {
                $obj=[];
                $obj['feid']=$item->feid;
-               $obj['count']=$i++;
+            //    $obj['count']=$i++;
                array_push($date_arr, $obj);
               }
                return $date_arr;
