382af3021fb0f3348b2cdee028683c3b99521ab0
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2019-10-30 14:50:47 +0500

    json is entering

--- app/Http/Controllers/Admin/AjaxNewController.php
+++ app/Http/Controllers/Admin/AjaxNewController.php
@@ -1778,7 +1778,9 @@ class AjaxNewController extends Controller
                 ->sum('log_in_hours_payable');
                 if($number_of_hours_sum > 286) {$number_of_hours_sum = 286;}
                 $number_of_hours_sum += $number_of_hours_sum ;
-                return $number_of_hours_sum;
+                return response()->json([
+                   'status'=>$number_of_hours_sum,
+                ]);
                 // $aed_trips_sum=Income_zomato::where('rider_id',$riders->rider_id)
                 // ->whereMonth('date',$month)
                 // ->sum('trips_payable');
