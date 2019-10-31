fa386a87a6fb40248da2906c2de8596c4da351ff
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2019-10-31 20:41:45 +0500

    all done

--- resources/views/Zomato_salary_sheet.blade.php
+++ resources/views/Zomato_salary_sheet.blade.php
@@ -29,12 +29,20 @@
                 <form class="kt-form" action="{{ route('bike.bike_create') }}" method="POST" enctype="multipart/form-data">
                     {{ csrf_field() }}
                     <div class="kt-portlet__body">
-                          Total Payout: <input type="text" class="form-control" id="total_payout">
-                          Salik: <input type="text" class="form-control" id="salik">
-                          Fuel: <input type="text" class="form-control" id="fuel">
-                          Sim: <input type="text" class="form-control" id="sim">
-                          Salary: <input type="text" class="form-control" id="salary">
-                          Profit: <input type="text" class="form-control" id="profit">
+                        Total Hours: <input type="text" class="form-control" id="hour">
+                        Total Trips: <input type="text" class="form-control" id="trip">
+                        AED Hours: <input type="text" class="form-control" id="aed_hour">
+                        AED Trips: <input type="text" class="form-control" id="aed_trips">
+                        Incentives: <input type="text" class="form-control" id="ncw">
+                        Tips: <input type="text" class="form-control" id="tips">
+                        Denial Penalty: <input type="text" class="form-control" id="penalty">
+                        DC Chrges & COD Amount: <input type="text" class="form-control" id="dc_cod">
+                        Total Payout: <input type="text" class="form-control" id="total_payout">
+                        Salik: <input type="text" class="form-control" id="salik">
+                        Fuel: <input type="text" class="form-control" id="fuel">
+                        Sim: <input type="text" class="form-control" id="sim">
+                        Salary: <input type="text" class="form-control" id="salary">
+                        Profit: <input type="text" class="form-control" id="profit">
                           
                     </div>
 
@@ -76,11 +84,29 @@
                         console.log(data);
                         $payout=data.payout;
                         $fuel=data.bike_fuel;
-                        $salik=(data.salik);
+                        $salik=data.salik;
                         $sim=data.sim;
                         $salary=data.salary;
-                        // $rent=data.bike_rent;
+                        $hour=data.total_hours;
+                        $trip=data.total_trips;
+                        $ncw=data.ncw;
+                        $tips=data.tips;
+                        $penalty=data.denials_penalty;
+                        $cod=data.cod;
+                        $dc_charges=data.DC_deduction;
+                        $aed_hour=$hour * 6;
+                        $aed_trip=$trip * 6.86;
+                        $dc_cod= $cod+$dc_charges;
                         $profit=$payout-($fuel+$salik+$sim+$salary);
+                        $('#hour').val($hour);
+                        $('#trip').val($trip);
+                        $('#aed_hour').val($aed_hour);
+                        $('#aed_trips').val($aed_trip);
+                        $('#ncw').val($ncw);
+                        $('#tips').val($tips);
+                        $('#penalty').val($penalty);
+                        $('#dc_cod').val($dc_cod);
+
                         $('#total_payout').val($payout);
                         $('#salik').val($salik);
                         $('#fuel').val($fuel);
