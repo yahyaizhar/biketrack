a9993c33e068688a09ca196c4427c11a5f685b77
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2019-11-01 19:37:08 +0500

    idhar bhi aa ja

--- resources/views/zomato_profit_sheet.blade.php
+++ resources/views/zomato_profit_sheet.blade.php
@@ -500,13 +500,13 @@ $(function() {
             '<td colspan="1"; style="font-weight:900;">Tips:</td>'+
             '<td colspan="2";>'+data.tips+'</td>'+
             '<td colspan="1"; style="font-weight:900;" >COD:</td>'+
-            '<td colspan="2";>'+'0'+'</td>'+
+            '<td colspan="2";>'+data.cod+'</td>'+
             '</tr>'+
             '<tr>'+
             '<td colspan="1"; style="font-weight:900;" >Denials Penalty:</td>'+
             '<td colspan="2";>'+data.penalty+'</td>'+
             '<td colspan="1"; style="font-weight:900;">DC:</td>'+
-            '<td colspan="2";>'+'0'+'</td>'+
+            '<td colspan="2";>'+data.dc+'</td>'+
             '<td colspan="1"; style="font-weight:900;" >Fuel:</td>'+
             '<td colspan="2";>'+'0'+'</td>'+
             '<td colspan="1"; style="font-weight:900;" >Salik:</td>'+
