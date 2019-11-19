a3e51a8727c9223b72249fb2311392aff0e3f493
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2019-11-19 20:20:21 +0500

    bills are tall se tall mila

--- app/Http/Controllers/Admin/AjaxNewController.php
+++ app/Http/Controllers/Admin/AjaxNewController.php
@@ -2392,7 +2392,7 @@ class AjaxNewController extends Controller
         return DataTables::of($bills)
         ->addColumn('date', function($bill){
             if (isset($bill->month)) {
-                return Carbon::parse($bill->month)->format('M, Y');
+                return Carbon::parse($bill->created_at)->format('M, Y');
             }
         })
         ->addColumn('bill', function($bill){
