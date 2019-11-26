01cfa97c7699f27261687394f2c94152f27ea4d1
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2019-11-26 17:34:30 +0500

    asaddas

---
+++ database/migrations/2019_11_26_123123_create_client__histories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client__histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('client_id')->nullable();
+            $table->integer('rider_id')->nullable();
+            $table->string('assign_date')->nullable();
+            $table->string('deassign_date')->nullable();
+            $table->string('status')->nullable();
+            $table->string('active_status')->default("A");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client__histories');
+    }
+}
