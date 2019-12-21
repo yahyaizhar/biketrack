8846dbedf401b85abb7f1350ba97c9957a1d3d4f
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2019-12-21 14:52:28 +0500

    saassaasassa

--- resources/views/accounts/add_new_salary.blade.php
+++ resources/views/accounts/add_new_salary.blade.php
@@ -276,7 +276,7 @@
                 var trips_amount=trips*2;
                 var extra_trips_amount=extra_trips*4;
                 var final_salary=trips_amount+hours_amount+extra_trips_amount;
-                console.log("gdsjhdsghaj"+extra_trips_amount);
+                console.log("gdsjhdsghaj"+data.trips_EXTRA_payable);
                 $('#salary [name="monthly_hours_val"]').val(data._s_monthlyHours);
                 $('#salary [name="absent_day_val"]').val(data.absent_count);
                 $('#salary [name="absent_hours_val"]').val((data.absent_count*data._s_maxHours).toFixed(2));
