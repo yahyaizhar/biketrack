bc857f00ce3d8982aa81f461517ef227ad3ed47c
yahyaizhar <yahyaizhar@gmail.com>
2019-12-31 16:03:01 +0500

    user permission revert back

--- app/Http/Middleware/Admin_Role.php
+++ app/Http/Middleware/Admin_Role.php
@@ -15,24 +15,22 @@ class Admin_Role
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next,$role)
     {
-        $route_name = $request->route()->getName();
         $admin_gaurd = $request->user();
-        // if($admin_gaurd->Active_status != "A"){
-        //     Auth::guard('admin')->logout();
-        //     return redirect('/admin/login');
-        // }
-        $user_type=$admin_gaurd->type;
-        if ($user_type=="normal") {
-            $users=$admin_gaurd->Role()->where("action_name",$route_name)->get()->first();
-            if (isset($users)) {
-                return $next($request);   
+        if($admin_gaurd->Active_status != "A"){
+            Auth::guard('admin')->logout();
+            return redirect('/admin/login');
+        }
+        if($role!="Custom_Auth"){
+            $user_type=$admin_gaurd->type;
+            if ($user_type=="normal") {
+                $users=$admin_gaurd->Role()->where("action_name",$role)->get()->first();
+                if (isset($users)) {
+                    return $next($request);   
+                }
+                return redirect(route('request.403'));
             }
-            if($request->ajax()){
-                return abort(403);
-            }
-            return redirect(route('request.403'));
         }
         return $next($request); 
      
