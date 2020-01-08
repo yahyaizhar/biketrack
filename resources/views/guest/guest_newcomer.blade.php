d9619acddacc3b5d7965aa539081631812c1b2de
arslan308 <arslansaleem308@gmail.com>
2020-01-08 15:50:57 +0500

    rr

--- resources/views/guest/guest_newcomer.blade.php
+++ resources/views/guest/guest_newcomer.blade.php
@@ -17,7 +17,7 @@
 
                 <!--begin::Form-->
                 
-                @include('admin.includes.message')
+                @include('admin.includes.message') 
                 <div class="alreay_registred" style="padding: 10px 25px;">
                                   <button class="btn btn-info custm_hidden_btn float-right" style=" margin-bottom: 10px;">Track your application</button>
                         <div class="hidde_status_form" style="display:none;">
