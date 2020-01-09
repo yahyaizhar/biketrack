36670ea11c19a54aa34971db37db7ae60946e3dd
yahyaizhar <yahyaizhar@gmail.com>
2020-01-09 16:58:25 +0500

    changes

--- app/Http/Middleware/Admin_Role.php
+++ app/Http/Middleware/Admin_Role.php
@@ -18,30 +18,21 @@ class Admin_Role
     public function handle($request, Closure $next)
     {
         $route_name = $request->route()->getName();
-        // dd($route_name);
         $admin_gaurd = $request->user();
-        // if($admin_gaurd->Active_status != "A"){
-        //     Auth::guard('admin')->logout();
-        //     return redirect('/admin/login');
-        // }
         $user_type=$admin_gaurd->type;
         if ($user_type=="normal") {
             $users=$admin_gaurd->Role()->where("action_name",$route_name)->get()->first();
             if (isset($users)) {
                 return $next($request);   
             }
-            $web_route=WebRoute::where("route_name",$route_name)->get()->first();
-            ///if route is not defined
-            if(!isset($web_route)){
-                return redirect(route('request.403'));
-            }
-            //end
-            $web_route_name = $web_route->label;
+            //permission denied
             if($request->ajax()){
+                $web_route=WebRoute::where("route_name",$route_name)->get()->first();
+                $web_route_name = $web_route->label;
                 return abort(403,$web_route_name); 
             }
+            return redirect(route('request.403'));  
         }
-        return $next($request); 
-     
+        return $next($request);
     }
 }
