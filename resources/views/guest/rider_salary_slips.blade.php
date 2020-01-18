252209235e85ae70a496097a1173099a427d718f
sohaibansari420 <53227378+sohaibansari420@users.noreply.github.com>
2020-01-18 18:46:46 +0500

    5656565656565

--- resources/views/guest/rider_salary_slips.blade.php
+++ resources/views/guest/rider_salary_slips.blade.php
@@ -23,7 +23,7 @@
                         <div class="row">
                             <div class="form-group col-md-9">
                                 <label>Emirate's ID:</label>
-                                <input type="text" class="form-control" name="emirate_id" placeholder="Enter Emirate's ID">
+                                <input type="text" autocomplete="off" class="form-control" name="emirate_id" placeholder="Enter Emirate's ID">
                             </div>
                             {{-- <div class="form-group">
                                 <label>Passport No':</label>
