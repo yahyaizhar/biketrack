8bb61a90b7f08134aa6767a4fceb5add2d29de6d
yahyaizhar <yahyaizhar@gmail.com>
2020-01-20 18:47:56 +0500

    asdasd

--- database/migrations/2019_07_15_111821_create_rider_details_table.php
+++ database/migrations/2019_07_15_111821_create_rider_details_table.php
@@ -40,7 +40,7 @@ class CreateRiderDetailsTable extends Migration
             $table->string('licence_image')->nullable();
             $table->string('licence_image_back')->nullable();
             $table->string('licence_expiry')->nullable();
-            $table->string('other_details')->nullable();
+            $table->text('other_details')->nullable();
             $table->string('show_salaryslip')->nullable();
             $table->string('salaryslip_month')->nullable();
             $table->string('salaryslip_expiry')->nullable();
